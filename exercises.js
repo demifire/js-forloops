704d2155b0a4748100ea2d0e168107629fd75bf5
Chaz Kondo <chuckeekondo@gmail.com>
2018-04-09 21:13:34 -1000

    Initial Commit

--- exercises.js
+++ exercises.js
@@ -1,3 +1,5 @@
+console.log('Hello World');
+
 /* 1) Ticket Generator
 Write a for-loop that will iterate through 20 numbers (starting at 1 and ending at 20) and console.log the following message:
 "Now serving 1."
@@ -9,6 +11,9 @@ Write a for-loop that will iterate through 20 numbers (starting at 1 and ending 
 "Now serving 20."
 */
 
+for (i = 1; i < 21; i++){
+console.log('Now serving ' + i)
+}
 
 /* 2) Pop Charts
 Write a for-loop that will iterate through the topFive array below and console.log the following message:
